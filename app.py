509e8de58632973b3de109eececf3eb36f95c13e
Shi <shijunx@b0be83560908.ant.amazon.com>
2022-07-20 18:10:36 +0800

    fix

--- app.py
+++ app.py
@@ -1,29 +1,44 @@
 from chalice import Chalice
+import boto3
+import logging
 
 app = Chalice(app_name='chalice-custom-label')
+app.log.setLevel(logging.DEBUG)
 
+rek = boto3.client('rekognition')
+sns = boto3.client('sns')
 
-@app.route('/')
-def index():
-    return {'hello': 'world'}
+bucket_name = 'so0050-128d09dcc3e5-348052051973-us-east-1-ingest'
+suffix = ('jpg', 'png')
 
+@app.on_s3_event(bucket=bucket_name,
+                 events=['s3:ObjectCreated:*'], prefix='input')
+def invoke_custom_label(event):
+    if not event.key.endswith(suffix):
+        return
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    app.log.debug("Received event for bucket: %s, key: %s",
+                  event.bucket, event.key)
+    s3uri = 's3://' + event.bucket + '/' + event.key
+    app.log.debug("s3uri: %s", s3uri)
+
+    response = rek.detect_custom_labels(
+        ProjectVersionArn='arn:aws:rekognition:us-east-1:348052051973:project/bayc2/version/bayc2.2022-06-02T17.38.48/1654162729074',
+        Image={
+            'S3Object': {
+                'Bucket': bucket_name,
+                'Name': event.key
+            }
+        },
+        MinConfidence=80.0
+    )
+    app.log.debug("Custom labels: %s", response['CustomLabels'])
+    detected_labels = [item['Name'] for item in response['CustomLabels']]
+    app.log.debug("Detected labels: %s", detected_labels)
+
+    response = sns.publish(
+        TopicArn='arn:aws:sns:us-east-1:348052051973:MyTopic',
+        Message=f'{event.key} is very similar to {detected_labels}, please have a look at {s3uri}',
+        Subject=f'Copy cat alert',
+    )
+    app.log.debug("response: %s", response)
